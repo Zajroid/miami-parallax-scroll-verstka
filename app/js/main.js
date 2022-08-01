a3392e63fdebb45193327527a459d6eb8bce227f
Danya <kloveazaks@gmail.com>
2022-08-01 23:10:03 +0300

    add filter on bg slider

--- app/js/main.js
+++ app/js/main.js
@@ -1,16 +1,52 @@
-const sliderMain = new Swiper('.slider_main', {
-	freeMode: true,
-	centeredSlides: true,
-	mousewheel: true,
-	parallax: true,
-	breakpoints: {
-		0: {
-			slidesPerView: 2.5,
-			spaceBetween: 20
-		},
-		680: {
-			slidesPerView: 2.5,
-			spaceBetween: 60
-		}
-	}
-})
+const sliderMain = new Swiper(".slider_main", {
+  freeMode: true,
+  centeredSlides: true,
+  mousewheel: true,
+  parallax: true,
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: 20,
+    },
+    680: {
+      slidesPerView: 2.2,
+      spaceBetween: 60,
+    },
+    1080: {
+        slidesPerView: 3.2,
+        spaceBetween: 80,
+    },
+    1440: {
+        slidesPerView: 3.6,
+        spaceBetween: 100,
+    },
+  },
+});
+
+const sliderBg = new Swiper(".slider_bg", {
+  centeredSlides: true,
+  parallax: true,
+  spaceBetween: 60,
+  slidesPerView: 3.5,
+
+  breakpoints: {
+    0: {
+      slidesPerView: 1.2,
+      spaceBetween: 60,
+    },
+    680: {
+      slidesPerView: 2.2,
+      spaceBetween: 100,
+    },
+    1080: {
+        slidesPerView: 3.2,
+        spaceBetween: 120,
+    },
+    1440: {
+        slidesPerView: 3.6,
+        spaceBetween: 140,
+    },
+  },
+});
+
+sliderMain.controller.control = sliderBg
